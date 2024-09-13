7c8bf67ca0aa9d32508846d6df7266bbb7e0859a
Megrez Lu <lujiajing1126@gmail.com>
2024-09-13 23:56:53 +0800

    fix versioning

--- scripts/deploy_kind.sh
+++ scripts/deploy_kind.sh
@@ -8,7 +8,7 @@ fi
 set -e
 
 make kustomize
-KUSTOMIZE=$(pwd)/bin/kustomize
+KUSTOMIZE=$(ls $(pwd)/bin/kustomize-* 2>/dev/null | head -n 1)
 (cd config/manager && "${KUSTOMIZE}" edit set image controller="${IMG}")
 "${KUSTOMIZE}" build config/default | sed -e 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' > /tmp/rollout-kustomization.yaml
 echo -e "resources:\n- manager.yaml" > config/manager/kustomization.yaml
